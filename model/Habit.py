1d6027521f65986affb65fd4bc0e8c1179e913b7
gazandic <gazandic@gmail.com>
2017-03-20 16:27:37 +0700

    model and util

---
+++ model/Habit.py
@@ -0,0 +1,87 @@
+# from flask import jsonify
+
+from BaseMongo import BaseMongo
+from datetime import datetime,date,time
+import pprint
+
+class Habit(BaseMongo):
+    def __init__(self, line_id, _about, _frequency, hh, mm, _fullfiled):
+        super().__init__()
+        self.lineid = line_id
+        self.about = _about
+        self.frequency = _frequency
+        self.time = time(hh, mm)
+        self.fullfiled = _fullfiled
+    # def setLineId(self, line_id):
+    #     self.lineid = line_id
+
+    # def setAbout(self, _about):
+    #     self.about = _about
+
+    def setFrequency(self, _frequency):
+        self.frequency = _frequency
+
+    def setTime(self, hh, mm):
+        self.time  = time(hh, mm)
+
+    def setFulfilled(self, _fullfiled):
+        self.fullfiled = _fullfiled
+
+    def getLineId(self):
+        return self.lineid
+
+    def getAbout(self):
+        return self.about
+
+    def getFrequency(self):
+        return self.frequency
+
+    def getTime(self):
+        return self.time
+
+    def getFullfiled(self):
+        return self.fullfiled
+
+    def create(self):
+        if self.searchOne(self.makeHabit()):
+            return 0
+        event_id = self.db.habits.insert_one(self.makeHabit()).inserted_id
+        return event_id
+
+    def update(self):
+        habit = self.searchOne({ "lineid" : self.lineid, "about" : self.about })
+        self.db.habits.update(
+            {'_id': habit['_id']},
+            { "$set": { "frequency" : self.frequency,"time" : self.time}},
+            upsert=False, multi=True)
+        return 0
+
+    def search(self, query):
+        habits =  self.db.habits.find(query)
+        return habits
+
+    def searchOne(self, query):
+        habit =  self.db.habits.find_one(query)
+        return habit
+
+    def removeSelf(self):
+        self.db.habits.delete_one(self.makeHabit())
+
+    def removeQuery(self, query):
+        self.db.habits.delete_many(query)
+
+    def makeHabit(self):
+        habit = { "lineid": self.lineid,
+                  "about" : self.about,
+                  "frequency" : self.frequency,
+                  "time" : self.time,
+                  "fullfiled" : self.fullfiled }
+        return habit
+
+ev1 = Habit("2783718371823718","shalat dhuhur",0,12,30)
+ev1.create()
+# ev1.removeSelf()
+# event = ev1.searchOne({"lineid":"2783718371823718"})
+# pprint.pprint(event)
+ev1.setFrequency(12)
+ev1.update()
---
+++ model/User.py
@@ -0,0 +1,90 @@
+# from flask import jsonify
+
+from BaseMongo import BaseMongo
+# from datetime import datetime,date,time
+import pprint
+
+class User(BaseMongo):
+    def __init__(self, line_id, _name, _location, _state):
+        super().__init__()
+        self.lineid = line_id
+        self.name = _name
+        self.location = _location
+        self.state = _state
+        # self.jointime = datetime.datetime.now()
+
+    # def setLineId(self, line_id):
+    #     self.lineid = line_id
+
+    def setName(self, _name):
+        self.name = _name
+
+    def setState(self, _state):
+        self.state = _state
+
+    def setLocation(self, _location):
+        self.location = _location
+
+    def getLineId(self):
+        return self.lineid
+
+    def getName(self):
+        return self.getName
+
+    def getState(self):
+        return self.state
+
+    def getLocation(self):
+        return self.location
+
+    def getJointime(self):
+        return self.jointime
+
+    def create(self):
+        if self.searchOne(self.makeUser()):
+            return 0
+        user_id = self.db.users.insert_one(self.makeUser()).inserted_id
+        return user_id
+
+    def update(self):
+        user = self.searchOne({ "lineid" : self.lineid})
+        self.db.users.update(
+            {'_id': user['_id']},
+            { "$set": { "name" : self.name,"location": self.location,"state" : self.state}},
+            upsert=False, multi=True)
+        return 0
+
+    def search(self, query):
+        users =  self.db.users.find(query)
+        return users
+
+    def searchOne(self, query):
+        user =  self.db.users.find_one(query)
+        return user
+
+    def set(self, user):
+        self.lineid = user['lineid']
+        self.name = user['name']
+        self.location = user['location']
+        self.state = user['state']
+
+    def removeSelf(self):
+        self.db.users.delete_one(self.makeUser())
+
+    def removeQuery(self, query):
+        self.db.users.delete_many(query)
+
+    def makeUser(self):
+        user = {"lineid": self.lineid,
+                "name" : self.name,
+                "location" : self.location,
+                "state" : self.state}
+        return user
+#
+# ev1 = User("2783718371823718","Osas",0)
+# ev1.create()
+# # ev1.removeSelf()
+# # event = ev1.searchOne({"lineid":"2783718371823718"})
+# # pprint.pprint(event)
+# ev1.setState(1)
+# ev1.update()
