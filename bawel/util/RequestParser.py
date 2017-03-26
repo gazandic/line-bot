cf53b461fd596e9835552ceb46da42f9f2bd7543
icalF <laser.survivor@gmail.com>
2017-03-26 13:55:54 +0700

    state parser separated

--- bawel/util/RequestParser.py
+++ bawel/util/RequestParser.py
@@ -2,96 +2,68 @@ from __future__ import unicode_literals
 
 from datetime import datetime,date,time
 
-import bawel.constant.UserConstant
-
 from bawel.action.Action import dispatch_action
-from bawel.action.Help import *
-from bawel.action.Jadwal import *
-from bawel.action.Pengeluaran import *
+from bawel.constant.StateConstant import *
 
-# from bawel.model.User import User
-# from bawel.model.Event import Event
-# from bawel.model.Expense import Expense
+from bawel.model.User import User
+from bawel.model.Event import Event
+from bawel.model.Expense import Expense
 
 class RequestParser:
-    self.command = {
-        '/help': HelpAll,
-        '/jadwal': HelpJadwal,
-        '/habit': HelpHabit,
-        '/pengeluaran': HelpPengeluaran,
-        '/tambahjadwal': TambahJadwal,
-        '/lihatjadwal': LihatJadwal,
-        '/ubahjadwal': UbahJadwal,
-        '/hapusjadwal': HapusJadwal,
-        '/selesaijadwal': SelesaiJadwal,
-        '/reportjadwal': ReportJadwal,
-        # '/tambahhabit': self.tambahhabitCommand,
-        # '/lihathabit': self.lihathabitCommand,
-        # '/ubahhabit': self.ubahhabitCommand,
-        # '/hapushabit': self.hapushabitCommand,
-        # '/selesaihabit': self.selesaihabitCommand,
-        # '/reporthabit': self.reporthabitCommand,
-        '/tambahpengeluaran': TambahPengeluaran,
-        '/lihatpengeluaran': LihatPengeluaran,
-        '/ubahpengeluaran': UbahPengeluaran,
-        '/hapuspengeluaran': HapusPengeluaran,
-        # '/selesaipengeluaran': self.selesaipengeluaranCommand,
-        '/reportpengeluaran': ReportPengeluaran
-    }
 
-    def parse(self, text):
-        lineid = self.state.id
-        us = User(lineid, "undefined", "undefined",
-                UserConstant.STATE_UNKNOWN_USERNAME)
-        user = us.searchOne({"lineid":lineid})
-        user = us.searchOne({"lineid":lineid})
+    def parse(self, text, state):
+        user = None
+        us = User(lineid, "undefined", "undefined", STATE_NOTHING)
+
+        if 'id' in state:
+            lineid = state.id
+            user = us.searchOne({"lineid":lineid})
+
         if not user:
             return us.create()
         us.set(user)
+
+        state = chooseMenu(text, state)
+        if state == STATE_UNKNOWN: return state
+            # print("halo bos, sekretaris bos "+bossName+ " kurang paham, coba ketik /help ya bos :)")
+
         if 'name' not in state or not state['name']:
-            return dispatch_action(UserPromptName, state)
+            state = {
+                state_id: STATE_ASK_USERNAME,
+                next_state: state
+            }
         elif 'loc' not in state or not state['loc']:
-            return dispatch_action(UserPromptLoc, state)
-        elif user['state'] == UserConstant.STATE_KNOWN_USER:
-            return self.chooseMenu(us)
+            state = {
+                state_id: STATE_ASK_LOC,
+                next_state: state
+            }
         else:
-            return self.default()
+            # TODO: hayo
+            pass
 
-    def default(self):
-        print("lol")
-
-    def chooseMenu(self, us):
-        cmd = self.text.split()
+    def chooseMenu(self, text, state):
+        cmd = text.split()
         param = cmd[1:]
         cmd = cmd[0]
-        bossName = str(self.us.getName())
+        bossName = state['name']
         param.append(bossName)
         try:
-            self.command[cmd](*param)
+            return REQUEST_STATE[cmd](*param)
         except IndexError:
-            print("halo bos, sekretaris bos "+bossName+ " kurang paham, coba ketik /help ya bos :)")
+            return STATE_UNKNOWN
 
-
-
-stp = StateTextParser("/help", "2783718371823718")
-stp.parse()
-stp.setText("/tambahjadwal date 10 3 2017 10 30 10")
-stp.parse()
-stp.setText("/lihatjadwal")
-stp.parse()
-stp.setText("/ubahjadwal date 30 4 2017 13 30 3")
-stp.parse()
-stp.setText("/lihatjadwal")
-stp.parse()
-stp.setText("/selesaijadwal date")
-stp.parse()
-stp.setText("/reportjadwal")
-stp.parse()
-stp.setText("/hapusjadwal date")
-stp.parse()
-stp.setText("/lihatjadwal")
-stp.parse()
-
-
-stp.setText("/lihatpengeluaran")
-stp.parse()
+# TEST SUITE
+#
+# stp = RequestParser()
+# stp.parse("/help", {})
+# stp.parse("/tambahjadwal date 10 3 2017 10 30 10", {})
+# stp.parse("/lihatjadwal", {})
+# stp.parse("/ubahjadwal date 30 4 2017 13 30 3", {})
+# stp.parse("/lihatjadwal", {})
+# stp.parse("/selesaijadwal date", {})
+# stp.parse("/reportjadwal", {})
+# stp.parse("/hapusjadwal date", {})
+# stp.parse("/lihatjadwal", {})
+#
+#
+# stp.parse("/lihatpengeluaran", {})
