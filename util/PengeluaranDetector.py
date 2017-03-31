b8bb752da708c3db3bac522e62e40aa4528f237e
Kevin Yauris <kyauris@gmail.com>
2017-03-31 19:07:04 +0700

    gommenasai

--- util/PengeluaranDetector.py
+++ util/PengeluaranDetector.py
@@ -1,6 +1,6 @@
 #nyoba pytesseract
 import re;
-from ImageProcessor import ImageProcessor
+from ImageProcessor import ImageProcessor;
 
 class PengeluaranDetector(object):
 	def __init__(self, imagePath):
@@ -15,6 +15,7 @@ class PengeluaranDetector(object):
 		if (self.IP.process_image(self.imagePath)):
 			temp = self.IP.process_image(self.imagePath);
 			lines = temp.splitlines();
+			totalCandidate =0;
 			for line in lines:
 				print(line);
 				temp_spended = []
@@ -33,19 +34,20 @@ class PengeluaranDetector(object):
 							if (not getByKeyWord):
 								getByKeyWord = True;
 						break;
-			if totalCandidate == 0 and not getByKeyWord :
-				searchSpended = re.compile(r'RP\D*((\d[\.,]|\d)+)', flags= re.IGNORECASE);
-				spended = searchSpended.findall(line);
-				if (spended):
-					for spent in spended:
-						print(spent);
-						spent = re.sub(r'([\.,]\d{2})\b','',spent[0]);
-						spent = re.sub(r'([\.|,])', "", spent);
-						print(spent);
-						spent = float(spent);
-						if totalCandidate < spent :
-							totalCandidate = spent;
+				if not getByKeyWord:
+					searchSpended = re.compile(r'RP\D*((\d[\.,]|\d)+)', flags= re.IGNORECASE);
+					spended = searchSpended.findall(line);
+					print(spended);
+					if (spended):
+						for spent in spended:
+							print(spent);
+							spent = re.sub(r'([\.,]\d{2})\b','',spent[0]);
+							spent = re.sub(r'([\.|,])', "", spent);
+							print(spent);
+							spent = float(spent);
+							if totalCandidate < spent :
+								totalCandidate = spent;
 			print ("totalnya adalah: ", totalCandidate);
 
-PD= PengeluaranDetector("tidakadarp.png");
+PD= PengeluaranDetector("contohgaz.jpg");
 PD.checkForTotal();
