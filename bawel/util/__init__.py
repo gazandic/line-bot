d9ac411cda33362424c6bf22e7c3d06f98143375
gazandic <gazandic@gmail.com>
2017-03-31 21:47:06 +0700

    server time

--- bawel/util/__init__.py
+++ bawel/util/__init__.py
@@ -1,6 +1,6 @@
 import requests, os
 
-from datetime import date, time, datetime
+from datetime import date, time, datetime, timedelta
 __all__ = ["checkInputWaktu", "checkInputTanggal", "ImageProcessor", "JsonToQuery", "PengeluaranDetector", "Reminder", "RequestParser", "Sticker", "TextProcessor", "CheckMoney"]
 
 def checkInputWaktu(jam, menit):
@@ -10,6 +10,9 @@ def checkInputTanggal(hari, bulan, tahun, jam, menit):
     d = date(int(tahun), int(bulan), int(hari))
     t = checkInputWaktu(jam, menit)
     dt = datetime.combine(d,t)
+    td = timedelta(hours=-7)
+    dt = dt + td
+    print(dt)
     return (dt)
 
 def searchLoc(query):
