61bfa70bdb416826294d3819b971f9152ad584b2
gazandic <gazandic@gmail.com>
2017-04-03 00:13:41 +0700

    jam 12 bug

--- bawel/util/__init__.py
+++ bawel/util/__init__.py
@@ -4,13 +4,13 @@ from datetime import date, time, datetime, timedelta
 __all__ = ["checkInputWaktu", "checkInputTanggal", "ImageProcessor", "JsonToQuery", "PengeluaranDetector", "Reminder", "RequestParser", "Sticker", "TextProcessor", "CheckMoney"]
 
 def checkInputWaktu(jam, menit):
-    return time(int(jam)-1, int(menit))
+    return time(int(jam), int(menit))
 
 def checkInputTanggal(hari, bulan, tahun, jam, menit):
     d = date(int(tahun), int(bulan), int(hari))
     t = checkInputWaktu(jam, menit)
     dt = datetime.combine(d,t)
-    td = timedelta(hours=-7)
+    td = timedelta(hours=-8)
     dt = dt + td
     print(dt)
     return (dt)
